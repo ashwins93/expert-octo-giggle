bebdaa269c65834b0469bd90ef90fe7e618c6fb3
Ashwin S <aswinjeganath@gmail.com>
2018-05-13 14:02:57 +0530

    setup react to work with router and add bootstrap

---
+++ interface/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch, Link
+} from 'react-router-dom';
+import SalesForm from './components/SalesForm';
+
+const App = () => (
+  <Router>
+    <div>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</ Link></li>
+          <li>
+            <Link to="/sales/new">New Order</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route exact path="/" render={() => <h1 className="title">Hello World</h1>} />
+        <Route exact path="/sales/new" component={SalesForm} />
+      </Switch>
+    </div>
+  </Router>
+);
+
+export default App;
---
+++ interface/src/components/SalesForm.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SalesForm = () => (
+  <div>
+    <h1>Sales</h1>
+  </div>
+);
+
+export default SalesForm;
